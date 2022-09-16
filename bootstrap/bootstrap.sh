475a3061db32fc73d81f8478fca45a059dd68df5
Steve Davidson <buzzdavidson@me.com>
2022-09-16 22:43:45 +0000

    adding script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,4 +3,7 @@
 echo "boostrapping needed packages; you may be asked to authenticate for sudo"
 sudo apt install aptitude ansible
 
+git config --global user.email "buzzdavidson@me.com"
+git config --global user.name "Steve Davidson"
+
 
